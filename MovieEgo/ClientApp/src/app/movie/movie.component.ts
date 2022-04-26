38a17db6b2169520c64f7beaf8b39856e9e026b7
Nate Walker <walkallthetime12@gmail.com>
2022-04-26 11:45:31 -0400

    plssssss

--- MovieEgo/ClientApp/src/app/movie/movie.component.ts
+++ MovieEgo/ClientApp/src/app/movie/movie.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MovieGetterService } from '../services/MovieGetterService';
 import { ActivatedRoute } from '@angular/router';
 import { MovieData } from '../interfaces/MovieData';
+import { PostService } from '../services/post.service';
 
 
 @Component({
@@ -15,14 +16,14 @@ export class MovieComponent {
   movie = {} as MovieData;
   poster_path_string = "https://image.tmdb.org/t/p/original/";
 
-  constructor(private GetterService: MovieGetterService, private activatedRoute: ActivatedRoute) {
+  constructor(private getterService: MovieGetterService, private activatedRoute: ActivatedRoute, private postService: PostService) {
     this.activatedRoute.paramMap.subscribe(params => {
       this.id = +params.get('id');
       this.movie.id = this.id;
     });
 
 
-    GetterService.getMovie(this.id).subscribe(result => {
+    getterService.getMovie(this.id).subscribe(result => {
       console.log(result);
       var temp = result as MovieData;
       this.movie.poster_path = this.poster_path_string + temp.poster_path;
@@ -32,6 +33,10 @@ export class MovieComponent {
       // var temp = result as MovieData;
       //temp.rating = "4.5"; // we'll need to retreive ratings from our DB
       // temp.poster_path = this.poster_path_string + temp.poster_path;
+    })
+
+    postService.getPostsByMovie(this.id).subscribe(result => {
+      console.log(result);
     })
   }
 
